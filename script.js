a391ba6fb71a05b27df247214dd4072224bc6b0c
Ryan Kamleiter <kamleiterryan@gmail.com>
2024-01-19 10:10:25 -0600

    fire button working as intended

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ onReady();
 function onReady() {
   console.log('Javascript is working!');
 }
+
+//create function for handleFireClick
+//adds the fire emoji to div element when clicked on
+
+function handleFireClick() {
+  let divIWantToAddTo = document.querySelector('div');
+  divIWantToAddTo.innerText += '🔥';
+
+}
+
